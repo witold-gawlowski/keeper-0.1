d8c5216b9b8f7307a7a6c559b5128724559b7e65
Witold Gawłowski <witek.gawlowski@gmail.com>
2020-12-28 14:29:56 +0100

    started snapping

---
+++ Assets/BlockManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    public static BlockManager instance;
+    List<BlockScript> blocks;
+    public List<BlockScript> Blocks
+    {
+        get { return blocks; }
+    }
+    private void Awake()
+    {
+        instance = this;   
+    }
+}
---
+++ Assets/SnappingScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnappingScript : MonoBehaviour
+{
+    List<GameObject> draggedLinkers;
+    List<GameObject> otherLinkers;
+    public float snappingDistance = 1.0f;
+
+    float minDistance;
+    GameObject aLinker, bLinker;
+    private void FixedUpdate()
+    {
+        foreach(GameObject a in draggedLinkers)
+        {
+            foreach(GameObject b in otherLinkers)
+            {
+                Vector3 abVector = a.transform.position - b.transform.position;
+                if(abVector.magnitude < minDistance)
+                {
+                    aLinker = a;
+                    bLinker = b;
+                    minDistance = abVector.magnitude;
+                }
+            }
+        }
+    }
+    public void OnDragStart(BlockScript block)
+    {
+        draggedLinkers = new List<GameObject>();
+        otherLinkers = new List<GameObject>();
+
+        foreach(Transform t in block.transform)
+        {
+            if(t.CompareTag("Linker"))
+            {
+                draggedLinkers.Add(t.gameObject);
+            }
+        }
+
+        List<BlockScript> otherBlocks = BlockManager.instance.Blocks;
+        foreach(BlockScript b in otherBlocks)
+        {
+            foreach(Transform t in b.transform)
+            {
+                if (t.CompareTag("Linker"))
+                {
+                    otherLinkers.Add(t.gameObject);
+                }
+            }
+        }
+    }
+}
